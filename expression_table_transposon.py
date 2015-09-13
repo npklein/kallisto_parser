2fca55faf98960a514d87df63fd21ff07659b421
NPK <niekdeklein@gmail.com>
2015-09-13 23:57:04 +0200

    change rows to genes, columns ot samples

--- expression_table_transposon.py
+++ expression_table_transposon.py
@@ -1,35 +1,30 @@
 # Niek de Klein
 # 13/09/2015
 import os
-import sys
+
 kallisto_rootdir = '/Users/NPK/UMCG/projects/PRO-seq_pipeline/PROseq_cd4_jurkat/gm/kallisto/'
 combined_tpm = open(os.path.join(kallisto_rootdir, 'tpm_combined.tsv'),'w')
 combined_est_counts = open(os.path.join(kallisto_rootdir, 'est_counts_combined.tsv'),'w')
 add_header = True
-
-table = {}
-header = ''
 for root, dirs, files in os.walk(kallisto_rootdir):
     for filename in files:
         if not filename == 'abundance.tsv':
             continue
         samplename = root.replace(kallisto_rootdir,'')
-        header += '\t'+samplename
+        header = ''
+        tpm = samplename
+        est_counts = samplename
         with open(os.path.join(root,filename)) as kallisto_file:
             kallisto_file.readline()
             for line in kallisto_file:
-                line = line.strip().split('\t')
-                if line[0] not in table:
-                    table[line[0]] = [[line[3], line[4]]]
-                else:
-                    table[line[0]].append([line[3],line[4]])
-combined_tpm.write(header)
-combined_est_counts.write(header)
-for gene in table:
-    combined_est_counts.write('\n'+gene)
-    combined_tpm.write('\n'+gene)
-    for sample in table[gene]:
-        combined_est_counts.write('\t'+sample[0])
-        combined_tpm.write('\t'+sample[1])
+                header += '\t'+line.split('\t')[0]
+                tpm += '\t'+line.strip().split('\t')[4]
+                est_counts += '\t'+line.split('\t')[3]
+            if add_header:
+                combined_tpm.write(header+'\n')
+                combined_est_counts.write(header+'\n')
+                add_header = False
+            combined_tpm.write(tpm+'\n')
+            combined_est_counts.write(est_counts+'\n')
 combined_tpm.close()
 combined_est_counts.close()
